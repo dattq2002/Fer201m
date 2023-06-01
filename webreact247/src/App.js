66f1a26cdbb757047bef78168872c539a4e3be2a
Trang Quoc Dat <merycome123@gmail.com>
2023-06-01 15:41:51 +0700

    Exercise10

--- webreact247/src/App.js
+++ webreact247/src/App.js
@@ -2,11 +2,13 @@ import Navigation from './components/Navigation';
 import './App.css';
 import Main from './components/Main';
 import Footer from './components/Footer';
+import Player from './components/Players';
 function App() {
   return (
     <div className='App'>
       <Navigation/>
-      <Main/>
+      {/* <Main/> */}
+      <Player/>
       <Footer/>
     </div>
   );
